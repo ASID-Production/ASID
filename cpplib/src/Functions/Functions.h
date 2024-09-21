b5932c0a757fe30d4ac19119a33409f6728f8770
Alexander Volodin <Robot2100@mail.ru>
2024-09-22 01:38:38 +0300

    update Calling Protocols: arguments and return values

--- cpplib/src/Functions/Functions.h
+++ cpplib/src/Functions/Functions.h
@@ -26,47 +26,53 @@ std::tuple<std::string, std::string, cpplib::FindMolecules::RightType>
 							 cpplib::currents::FAMStructType::PointConteinerType& points);
 
 
-std::string FindDistanceWC(cpplib::currents::FAMStructType::AtomContainerType& types,
-						   cpplib::currents::FAMStructType::PointConteinerType& points,
-						   const std::array<int, 2>& type,
-						   const std::pair<float, float>& value);
+std::vector<cpplib::currents::FindGeometryType::tupleDistance> 
+	FindDistanceWC(cpplib::currents::FAMStructType::AtomContainerType& types,
+				   cpplib::currents::FAMStructType::PointConteinerType& points,
+				   const std::array<int, 2>& type,
+				   const std::pair<float, float>& value);
 
-std::string FindDistanceIC(const std::array<float, 6>& unit_cell,
-						   std::vector<const char*>& symm,
-						   cpplib::currents::FAMStructType::AtomContainerType& types,
-						   cpplib::currents::FAMStructType::PointConteinerType& points,
-						   const std::array<int, 2>& type,
-						   const std::pair<float, float>& value);
+std::vector<cpplib::currents::FindGeometryType::tupleDistance>
+	FindDistanceIC(const std::array<float, 6>& unit_cell,
+				   std::vector<const char*>& symm,
+				   cpplib::currents::FAMStructType::AtomContainerType& types,
+				   cpplib::currents::FAMStructType::PointConteinerType& points,
+				   const std::array<int, 2>& type,
+				   const std::pair<float, float>& value);
 
-std::string FindAngleWC(cpplib::currents::FAMStructType::AtomContainerType& types,
-						cpplib::currents::FAMStructType::PointConteinerType& points,
-						const std::array<int, 3>& type,
-						const std::array<std::pair<float, float>, 2>& value_d,
-						const std::pair<float, float>& value_a);
+std::vector<cpplib::currents::FindGeometryType::tupleAngle> 
+	FindAngleWC(cpplib::currents::FAMStructType::AtomContainerType& types,
+				cpplib::currents::FAMStructType::PointConteinerType& points,
+				const std::array<int, 3>& type,
+				const std::array<std::pair<float, float>, 2>& value_d,
+				const std::pair<float, float>& value_a);
 
-std::string FindAngleIC(const std::array<float, 6>& unit_cell,
-						std::vector<const char*>& symm, 
-						cpplib::currents::FAMStructType::AtomContainerType& types,
-						cpplib::currents::FAMStructType::PointConteinerType& points,
-						const std::array<int, 3>& type,
-						const std::array<std::pair<float, float>, 2>& value_d,
-						const std::pair<float, float>& value_a);
+std::vector<cpplib::currents::FindGeometryType::tupleAngle> 
+	FindAngleIC(const std::array<float, 6>& unit_cell,
+				std::vector<const char*>& symm, 
+				cpplib::currents::FAMStructType::AtomContainerType& types,
+				cpplib::currents::FAMStructType::PointConteinerType& points,
+				const std::array<int, 3>& type,
+				const std::array<std::pair<float, float>, 2>& value_d,
+				const std::pair<float, float>& value_a);
 
-std::string FindTorsionWC(cpplib::currents::FAMStructType::AtomContainerType& types,
-						  cpplib::currents::FAMStructType::PointConteinerType& points,
-						  const std::array<int, 4>& type,
-						  const std::array<std::pair<float, float>, 3>& value_d,
-						  const std::array<std::pair<float, float>, 2>& value_a,
-						  const std::pair<float, float>& value_t);
+std::vector<cpplib::currents::FindGeometryType::tupleTorsion> 
+	FindTorsionWC(cpplib::currents::FAMStructType::AtomContainerType& types,
+				  cpplib::currents::FAMStructType::PointConteinerType& points,
+						                                  const std::array<int, 4>& type,
+				  const std::array<std::pair<float, float>, 3>& value_d,
+				  const std::array<std::pair<float, float>, 2>& value_a,
+				  const std::pair<float, float>& value_t);
 
-std::string FindTorsionIC(const std::array<float, 6>& unit_cell,
-						  std::vector<const char*>& symm, 
-						  cpplib::currents::FAMStructType::AtomContainerType& types,
-						  cpplib::currents::FAMStructType::PointConteinerType& points,
-						  const std::array<int, 4>& type,
-						  const std::array<std::pair<float, float>, 3>& value_d,
-						  const std::array<std::pair<float, float>, 2>& value_a,
-						  const std::pair<float, float>& value_t);
+std::vector<cpplib::currents::FindGeometryType::tupleTorsion> 
+	FindTorsionIC(const std::array<float, 6>& unit_cell,
+				  std::vector<const char*>& symm, 
+				  cpplib::currents::FAMStructType::AtomContainerType& types,
+				  cpplib::currents::FAMStructType::PointConteinerType& points,
+				  const std::array<int, 4>& type,
+				  const std::array<std::pair<float, float>, 3>& value_d,
+				  const std::array<std::pair<float, float>, 2>& value_a,
+				  const std::pair<float, float>& value_t);
 
 
 cpplib::DATTuple FindDAT_IC(const std::array<float, 6>& unit_cell,
